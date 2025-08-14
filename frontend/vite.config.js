faa7ad282a8526309fab36a37473a1484d00a23d
setioryski <setioryski@gmail.com>
2025-08-14 18:21:16 +0700

    make hardcode login1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     proxy: {
       // string shorthand: http://localhost:5173/api -> http://localhost:3000/api
       '/api': 'http://localhost:3000',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        invoice: resolve(__dirname, 'invoice.html'),
+      },
     },
   },
 });
